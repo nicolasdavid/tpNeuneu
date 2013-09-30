52ed1c141470ca37137ca0b45973556d2a1f857e
Nicolas DAVID <nicolas.david@eleves.ec-nantes.fr>
2013-09-30 11:40:06 +0200

    creation of the nourriture constructor - loft constructor is ready -

--- src/tpneuneu/Nourriture.java
+++ src/tpneuneu/Nourriture.java
@@ -1,15 +1,18 @@
 package tpneuneu;
 
-import java.util.Vector;
-
 public class Nourriture implements Mangeable {
 
-  protected int name;
-
-  protected String type;
+  protected int type;
 
   protected int niveau;
+  
+  public Nourriture(int qte){
+      this.niveau=qte;
+      this.type=(int)Math.random()*2;
+     
+  }
 
+  @Override
   public void setNiveau(int i) {
   }
 
