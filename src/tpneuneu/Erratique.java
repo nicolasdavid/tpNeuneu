982732791201e3b5d19a9c4d4cccf29037ca5c90
Sebastien COHENDET <Sebastien.cohendet@centraliens-nantes.net>
2013-09-30 11:59:25 +0200

    Manger method implemented at 90%

--- src/tpneuneu/Erratique.java
+++ src/tpneuneu/Erratique.java
@@ -17,6 +17,7 @@ public class Erratique extends Neuneu {
   }
   @Override
   public void seDeplacer() {
+      
   }
 
 }
--- src/tpneuneu/Neuneu.java
+++ src/tpneuneu/Neuneu.java
@@ -15,9 +15,29 @@ public abstract class Neuneu implements Mangeable{
   protected static int nameId=-1;
 
   public void setNiveau(int i) {
+      this.niveau = i;
   }
-
+  
+    /**
+     * 
+     * @param repas repas à manger
+     */
   public void manger(Mangeable repas) {
+      if (repas instanceof Neuneu) { //if eat a Neuneu
+          repas.setNiveau(0);
+          this.setNiveau(100); //set energie of neuneu eaten to 0 (as dead) and to 100 for the eater
+      }
+      else{ //if eat a nourriture
+          int niveauRestantNourriture = repas.niveau-this.niveau;
+          if(niveauRestantNourriture<0){
+                repas.setNiveau(0); //no more nourriture
+                this.setNiveau(??); //TODO pas bon vca
+          }
+          else{
+                repas.setNiveau(niveauRestantNourriture);
+                this.setNiveau(this.niveau+niveauRestantNourriture);
+          }
+      }
   }
 
   public void seReproduire() {
@@ -28,10 +48,14 @@ public abstract class Neuneu implements Mangeable{
   public void majPresence(Case c) {
   }
 
-  public void newOperation() {
-  }
-
+  /**
+   * 
+   */
   public void plusProcheVoisin() {
+      
+      do {          
+          
+      } while (true);
   }
 
   public void plusProcheNourriture() {
