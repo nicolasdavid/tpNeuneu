b7b19e434a22a1faf9f3a781d5077feff254519a
Sebastien COHENDET <Sebastien.cohendet@centraliens-nantes.net>
2013-10-07 14:58:47 +0200

    Fix issue in cannibale eating

--- src/tpneuneu/Cannibale.java
+++ src/tpneuneu/Cannibale.java
@@ -30,30 +30,20 @@ public class Cannibale extends Vorace {
         int bouffeInt = repas.size();
         
         if (bouffeInt > 0){
-            int intABouffer=0, pasManger=0;
-            //Case of none cannibale so we search the first not neuneu in ListePresence
-                while (intABouffer<bouffeInt && repas.get(intABouffer) instanceof Neuneu){
-                    intABouffer++;
-                }
-                if (intABouffer >= bouffeInt){ //no Nourriture, only Neuneu on the case so skip the manger function
-                    pasManger = 1;
-                }
-              
-            if (pasManger==0){
+            int intABouffer=0;
                 
-                if (repas.get(intABouffer) instanceof Neuneu) { //if eat a Neuneu
-                    repas.get(intABouffer).setNiveau(0);
-                    this.setNiveau(100); //set energie of neuneu eaten to 0 (as dead) and to 100 for the eater
-                }
-                else if(repas.get(intABouffer).niveau+this.niveau>100){ //if eat a nourriture
-                      repas.get(intABouffer).setNiveau(100-repas.get(intABouffer).niveau+this.niveau); //set new niveau
-                      this.setNiveau(100); //Neuneu has full energy
-                }
-                else{
-                    this.setNiveau(this.niveau+repas.get(intABouffer).niveau);  
-                    repas.get(intABouffer).setNiveau(0); //no more food           
-                }
-            }   
+            if (repas.get(intABouffer) instanceof Neuneu) { //if eat a Neuneu
+                repas.get(intABouffer).setNiveau(0);
+                this.setNiveau(100); //set energie of neuneu eaten to 0 (as dead) and to 100 for the eater
+            }
+            else if(repas.get(intABouffer).niveau+this.niveau>100){ //if eat a nourriture
+                  repas.get(intABouffer).setNiveau(100-repas.get(intABouffer).niveau+this.niveau); //set new niveau
+                  this.setNiveau(100); //Neuneu has full energy
+            }
+            else{
+                this.setNiveau(this.niveau+repas.get(intABouffer).niveau);  
+                repas.get(intABouffer).setNiveau(0); //no more food           
+            }
         }
   }
    
